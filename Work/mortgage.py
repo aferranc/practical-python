9202a51d6d786f8375b99211b15236edd5f250bc
Albert Ferran Casas <albert.ferran@gmail.com>
2022-12-28 16:31:22 +0100

    Solved

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,23 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+total_paid = 0.0
+month = 0
+extra_payment_start_month = float(input("First month: "))
+extra_payment_end_month = float(input("Last month: "))
+extra_payment = float(input("Payment amount: "))
+
+
+while principal > 0:
+    month += 1
+    payment = 2684.11
+    if extra_payment_start_month <= month <= extra_payment_end_month:
+        payment += extra_payment
+    
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+
+print(f"Total paid {round(total_paid, 2)} over {month} months")
