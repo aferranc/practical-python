a50ab6c93f0ab76bf41abdf1c06226d6460de106
Albert Ferran Casas <albert.ferran@gmail.com>
2023-01-02 12:25:30 +0100

    Solved

--- Work/stock.py
+++ Work/stock.py
@@ -10,6 +10,15 @@ class Stock:
     def cost(self):
         return self.shares * self.price
 
-    def sell(self, amount):
-        self.shares -= amount
-        return self.shares
+    def sell(self, nshares):
+        self.shares -= nshares
+
+
+class MyStock(Stock):
+    def __init__(self, name, shares, price, factor):
+        # Check the call to `super` and `__init__`
+        super().__init__(name, shares, price)
+        self.factor = factor
+
+    def cost(self):
+        return self.factor * super().cost()
